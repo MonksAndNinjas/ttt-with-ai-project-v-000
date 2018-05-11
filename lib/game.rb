7eab7dadd6a1e775253e0184d301dd5eaebce54c
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-11 17:26:20 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,7 @@ class Game
   attr_accessor :board, :player_1, :player_2
   ::WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
-  def initialize(player_1 = Human.new("X"), player_2 = Human.new("O"), board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
@@ -50,7 +50,7 @@ class Game
   end
 
   def play
-    while self.over? == false
+    while self.over?
       self.turn
     end
 
