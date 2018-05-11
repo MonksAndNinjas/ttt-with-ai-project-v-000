fc055b008e42b63f0a2c12228cf52af48609d63a
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-11 17:00:02 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,4 @@
 
-
 class Board
   attr_accessor :cells
   ::WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
@@ -85,3 +84,5 @@ class Board
      self.cells[self.won?[0]] if self.won? != nil
    end
 end
+
+
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,6 +7,7 @@ module Players
 
 
       def move(board)
+<<<<<<< HEAD
         pos_num = self.minmax(board, self)
         @best_choice[0].to_s
       end
@@ -14,6 +15,15 @@ module Players
       def minmax(board, current_player)
         if board.won? != nil or board.full? == true
           return score(board)
+=======
+        pos_num = minmax(board, self)
+        pos_num
+      end
+
+      def minmax(board, current_player)
+        if board.over? == true
+          return scores(board)
+>>>>>>> 19bb005385aa1fc0df559fd1ea45423209725191
         end
 
         scores = {}     #{ :position_number => }
@@ -25,7 +35,11 @@ module Players
           scores[pos_num] = self.minmax(potential_board, self.switch(current_player))
         end
 
+<<<<<<< HEAD
         @best_choice, best_score = self.best_move(current_player, scores)
+=======
+        best_score = self.best_move(current_player, scores)
+>>>>>>> 19bb005385aa1fc0df559fd1ea45423209725191
         best_score
       end
 
@@ -33,7 +47,10 @@ module Players
         if current_player.class == String
           if current_player == self.token
             scores.max_by { |_k, v| v }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 19bb005385aa1fc0df559fd1ea45423209725191
           else
             scores.min_by { |_k, v| v }
           end
@@ -48,10 +65,17 @@ module Players
       end
 
       def score(board)
+<<<<<<< HEAD
         if board.winner == self.token
           10
         elsif board.winner == self.switch(self)
           -10
+=======
+        if board.winner == piece
+          return 10
+        elsif board.winner == @opponent
+          return -10
+>>>>>>> 19bb005385aa1fc0df559fd1ea45423209725191
         end
         0
       end
